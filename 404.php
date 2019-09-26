a50ad94ace482f44f9fdbfabaebad2452800227b
U-DESKTOP-L3FMDUM\ibanp <ibanpallano1997@gmail.com>
2019-09-25 21:48:13 -0400

    Colocamos nuevos atributos roles y hicimos algunos ajustes en css

--- 404.php
+++ 404.php
@@ -11,21 +11,21 @@
 </head>
 <body>
     <header role="Titulo">
-        <h1><a href="index.php">SmallText</a></h1>
+        <h1><a href="index.php" role="link-inicio">SmallText</a></h1>
     </header>
     <main>
         <section>
             <article role="Contenido-404">
-                <h2>Upss! ha ocurrido un error, direccion url no encontrada.</h2>
+                <h2 role="titulo">Upss! ha ocurrido un error, direccion url no encontrada.</h2>
                 <img src="img/404.png" title="Error 404" alt="Error 404, direccion no encontrada">
-                <h3>404</h3>
+                <h3 role="tipo-de-error">404</h3>
             </article>
         </section>
     </main>
-    <footer>
-        <p>Todos los derechos reservados. SmallText &copy;<?php echo date('Y'); ?></p>
+    <footer role="pie-de-pagina">
+        <p role="copyright">Todos los derechos reservados. SmallText &copy;<?php echo date('Y'); ?></p>
         <address>Desarrollado con <span>&hearts;</span> amor por <a href="https://www.facebook.com/iban.pallano.3"
-         target="_blank" rel="noopener noreferrer">Braylin Ivan Payano</a></address>
+         target="_blank" rel="noopener noreferrer" role="link-contacto">Braylin Ivan Payano</a></address>
     </footer>
     
 </body>
--- index.php
+++ index.php
@@ -20,25 +20,25 @@ require_once 'backend/index.php';
         <h1><a href="index.php">SmallText</a></h1>
     </header>
     <main role="contenido">
-        <section>
-            <article>
+        <section role="seccion">
+            <article role="articulo">
                 <h2 role="subtitulo">Calcula el texto de tu sitio web</h2>
                 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" role="formulario">
-                    <textarea name="text" autofocus required
+                    <textarea name="text" autofocus required role="caja-de-texto"
                     placeholder="Pega aqui el texto para que nuestro robots lo examine"><?php if(isset($_POST["submit"])){echo $text;} ?></textarea><br/>
-                    <button type="submit" name="submit">Examinar</button>
-                    <button type="reset">Limpiar</button>
+                    <button type="submit" name="submit" role="enviar-formulario">Examinar</button>
+                    <button type="reset" role="limpiar-formulario">Limpiar</button>
                 </form>
 
                 <?php if(isset($success)) : ?>
                     <div class="messeger" role="informacion">
-                        <strong><?php if(isset($text)) {echo $palabra . " - Palabras <br/>";} ?></strong>
-                        <strong><?php if(isset($text)) {echo $letras . " - Letras <br/>";} ?></strong>
-                        <strong><?php if(isset($text)) {echo count(cantidad_párrafos("$text")) . " - Párrafos <br/>";} ?></strong>
+                        <strong role="palabras"><?php if(isset($text)) {echo $palabra . " - Palabras <br/>";} ?></strong>
+                        <strong role="letras"><?php if(isset($text)) {echo $letras . " - Letras <br/>";} ?></strong>
+                        <strong role="parrafos"><?php if(isset($text)) {echo count(cantidad_párrafos("$text")) . " - Párrafos <br/>";} ?></strong>
                     </div><br/>
                 <?php endif; ?>
                         <?php if(isset($errors)) : ?>
-                        <b><?php echo $errors; ?></b>
+                        <b role="mensaje-error"><?php echo $errors; ?></b>
                 <?php endif; ?>
 
             </article>
@@ -46,7 +46,7 @@ require_once 'backend/index.php';
     </main>
     <footer role="pie-pagina">
         <p role="copyringht">Todos los derechos reservados. SmallText &copy;<?php echo date('Y'); ?></p>
-        <address role="contacto">Desarrollado con <span>&hearts;</span> amor por <a role="direccion" href="https://www.facebook.com/iban.pallano.3"
+        <address role="contacto">Desarrollado con <span role="corazon">&hearts;</span> amor por <a role="direccion" href="https://www.facebook.com/iban.pallano.3"
          target="_blank" rel="noopener noreferrer">Braylin Ivan Payano</a></address>
     </footer>
     
