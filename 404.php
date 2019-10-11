976aad9917c1504f9527a6d0aaa7e11fec17b10b
U-DESKTOP-L3FMDUM\ibanp <ibanpallano1997@gmail.com>
2019-10-11 15:53:36 -0400

    Eliminamos atributos role para mejorar semantica

--- 404.php
+++ 404.php
@@ -18,22 +18,22 @@
     <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
 </head>
 <body>
-    <header role="Titulo">
-        <h1><a href="index.php" role="link-inicio">SmallText</a></h1>
+    <header>
+        <h1><a href="index.php">SmallText</a></h1>
     </header>
     <main>
         <section>
-            <article role="Contenido-404">
-                <h2 role="titulo">Upss! ha ocurrido un error, direcci&oacute;n url no encontrada.</h2>
+            <article>
+                <h2>Upss! ha ocurrido un error, direcci&oacute;n url no encontrada.</h2>
                 <img src="img/404.png" title="Error 404" alt="Error 404, direccion no encontrada">
-                <h3 role="tipo-de-error">404</h3>
+                <h3>404</h3>
             </article>
         </section>
     </main>
-    <footer role="pie-de-pagina">
-        <p role="copyright">Todos los derechos reservados. SmallText &copy;<?php echo date('Y'); ?></p>
+    <footer>
+        <p>Todos los derechos reservados. SmallText &copy;<?php echo date('Y'); ?></p>
         <address>Desarrollado con <span>&hearts;</span> amor por: <a href="https://www.facebook.com/iban.pallano.3"
-         target="_blank" rel="noopener noreferrer" role="link-contacto">Braylin Ivan Payano</a></address>
+         target="_blank" rel="noopener noreferrer">Braylin Ivan Payano</a></address>
     </footer>
     
 </body>
--- index.php
+++ index.php
@@ -29,37 +29,37 @@ require_once 'backend/index.php';
     <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
 </head>
 <body>
-    <header role="Titulo">
+    <header>
         <h1><a href="index.php">SmallText</a></h1>
     </header>
-    <main role="contenido">
-        <section role="seccion">
-            <article role="articulo">
-                <h1 role="subtitulo">Calcula el texto de tu sitio web</h1>
-                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" role="formulario">
-                    <textarea name="text" autofocus required role="caja-de-texto"
+    <main>
+        <section>
+            <article>
+                <h2>Calcula el texto de tu sitio web</h2>
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+                    <textarea name="text" autofocus required
                     placeholder="Pega aqui el texto para que nuestro robot lo examine"><?php if(isset($_POST["submit"])){echo $text;} ?></textarea><br/>
-                    <button type="submit" name="submit" role="enviar-formulario">Examinar</button>
-                    <button type="reset" role="limpiar-formulario">Limpiar</button>
+                    <button type="submit" name="submit">Examinar</button>
+                    <button type="reset">Limpiar</button>
                 </form>
 
                 <?php if(isset($success)) : ?>
-                    <div class="messeger" role="informacion">
-                        <strong role="palabras"><?php if(isset($text)) {echo $palabra . " - Palabras <br/>";} ?></strong>
-                        <strong role="letras"><?php if(isset($text)) {echo $letras . " - Letras <br/>";} ?></strong>
-                        <strong role="parrafos"><?php if(isset($text)) {echo count(cantidad_párrafos("$text")) . " - Párrafos <br/>";} ?></strong>
+                    <div class="messeger">
+                        <strong><?php if(isset($text)) {echo $palabra . " - Palabras <br/>";} ?></strong>
+                        <strong><?php if(isset($text)) {echo $letras . " - Letras <br/>";} ?></strong>
+                        <strong><?php if(isset($text)) {echo count(cantidad_párrafos("$text")) . " - Párrafos <br/>";} ?></strong>
                     </div><br/>
                 <?php endif; ?>
                         <?php if(isset($errors)) : ?>
-                        <b role="mensaje-error"><?php echo $errors; ?></b>
+                        <b><?php echo $errors; ?></b>
                 <?php endif; ?>
 
             </article>
         </section>
     </main>
-    <footer role="pie-pagina">
-        <p role="copyringht">Todos los derechos reservados. SmallText &copy;<?php echo date('Y'); ?></p>
-        <address role="contacto">Desarrollado con <span role="corazon">&hearts;</span> amor por: <a role="direccion" href="https://www.facebook.com/iban.pallano.3"
+    <footer>
+        <p>Todos los derechos reservados. SmallText &copy;<?php echo date('Y'); ?></p>
+        <address>Desarrollado con <span>&hearts;</span> amor por: <a href="https://www.facebook.com/iban.pallano.3"
          target="_blank" rel="noopener noreferrer">Braylin Ivan Payano</a></address>
     </footer>
     
